3721b3c7cbc817e757748f80eeb85f553397ec05
EduardoVisconti <duduhsg@gmail.com>
2022-06-02 19:14:48 -0500

    aula01
    changind the title and doing the math for imc

---
+++ js/principal.js
@@ -0,0 +1,15 @@
+const titulo = document.querySelector('.titulo');
+titulo.textContent = 'Aparecida Nutricionista';
+
+const paciente = document.querySelector("#primeiro-paciente");
+
+const tdPeso = paciente.querySelector(".info-peso");
+const peso = tdPeso.textContent; //Pegando o conteúdo dele = 100.
+
+const tdAltura = paciente.querySelector(".info-altura");
+const altura = tdAltura.textContent;
+
+const tdImc = paciente.querySelector(".info-imc")
+const imc = peso / (altura * altura);
+
+tdImc.textContent = imc;
