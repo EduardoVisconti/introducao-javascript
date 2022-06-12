bd106d5858f905cea394455d382f7e38ad7068bd
Eduardo Visconti <duduhsg@gmail.com>
2022-06-12 11:35:19 -0700

    Update filtra.js

--- js/filtra.js
+++ js/filtra.js
@@ -1,6 +1,34 @@
 const campoFiltro = document.querySelector('#filtrar-tabela')
 
 campoFiltro.addEventListener('input', function () {
+  const pacientes = document.querySelectorAll('.paciente')
+
+  if (this.value.length > 0) {
+    for (let i = 0; i < pacientes.length; i++) {
+      const paciente = pacientes[i]
+      const tdNome = paciente.querySelector('.info-nome')
+      const nome = tdNome.textContent
+      const expressao = new RegExp(this.value, 'i')
+
+      if (!expressao.test(nome)) {
+        paciente.classList.add('invisivel')
+      } else {
+        paciente.classList.remove('invisivel')
+      }
+    }
+  } else {
+    for (let i = 0; i < pacientes.length; i++) {
+      const paciente = pacientes[i]
+      paciente.classList.remove('invisivel')
+    }
+  }
+})
+
+/* 
+
+const campoFiltro = document.querySelector('#filtrar-tabela')
+
+campoFiltro.addEventListener('input', function () {
   console.log(this.value)
   const pacientes = document.querySelectorAll('.paciente')
 
@@ -22,3 +50,5 @@ campoFiltro.addEventListener('input', function () {
     }
   }
 })
+
+*/
